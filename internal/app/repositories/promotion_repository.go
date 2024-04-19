b0a886ae06125d3e77352b6702c741b8849e6e3c
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-04-19 22:26:54 +0700

    make repository contract

---
+++ internal/app/repositories/promotion_repository.go
@@ -0,0 +1,11 @@
+package repositories
+
+import "github.com/ardwiinoo/promotion-api/internal/app/models"
+
+type PromotionRepository interface {
+	CreatePromotion(promo models.Promotion) (models.Promotion, error)
+	GetAllPromotions() ([]models.Promotion, error)
+	GetPromotionbyPromotionID(promotionID string) (models.Promotion, error)
+	UpdatePromotionbyPromotionID(promo models.Promotion) (models.Promotion, error)
+	DeletePromotionbyPromotionID(promotionID string) error
+}
