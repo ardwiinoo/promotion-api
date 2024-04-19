73b20208c4c62f7f8a05fcf14b9d701e3f0660cd
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-04-19 22:27:24 +0700

    make service contract

---
+++ internal/app/services/promotion_postgres_service.go
@@ -0,0 +1,11 @@
+package services
+
+import "github.com/ardwiinoo/promotion-api/internal/app/models"
+
+type PromotionService interface {
+	CreatePromotion(promo models.Promotion) (models.Promotion, error)
+	GetAllPromotions() ([]models.Promotion, error)
+	GetPromotionbyPromotionID(promotionID string) (models.Promotion, error)
+	UpdatePromotionbyPromotionID(promo models.Promotion) (models.Promotion, error)
+	DeletePromotionbyPromotionID(promotionID string) error
+}
