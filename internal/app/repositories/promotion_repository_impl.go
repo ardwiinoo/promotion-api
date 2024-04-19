8160f8e2c9c83e83768a4cbbc0503032b5ee2ac3
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-04-19 22:26:37 +0700

    make repository impl, make create new instance func, and implement repository members

---
+++ internal/app/repositories/promotion_repository_impl.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"github.com/ardwiinoo/promotion-api/internal/app/models"
+	"gorm.io/gorm"
+)
+
+type PromotionRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewPromotionRepository(db *gorm.DB) PromotionRepository {
+	return &PromotionRepositoryImpl {
+		db: db,
+	}
+}
+
+func (repository *PromotionRepositoryImpl) CreatePromotion(promo models.Promotion) (models.Promotion, error) {
+	panic("implement me!")
+}
+
+func (r *PromotionRepositoryImpl) GetAllPromotions() ([]models.Promotion, error) {
+	panic("implement me!")
+}
+
+func (r *PromotionRepositoryImpl) GetPromotionbyPromotionID(PromotionID string) (models.Promotion, error) {
+	panic("implement me!")
+}
+
+func (r *PromotionRepositoryImpl) UpdatePromotionbyPromotionID(promo models.Promotion) (models.Promotion, error) {
+	panic("implement me!")
+}
+
+func (r *PromotionRepositoryImpl) DeletePromotionbyPromotionID(promotionID string) error {
+	panic("implement me!")
+}
