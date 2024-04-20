4af08d29413981de32870652c2d742ef9b46546e
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-04-20 21:14:54 +0700

    make service impl

--- internal/app/services/promotion_postgres_service_impl.go
+++ internal/app/services/promotion_postgres_service_impl.go
@@ -1,1 +1,36 @@
 package services
+
+import (
+	"github.com/ardwiinoo/promotion-api/internal/app/models"
+	"github.com/ardwiinoo/promotion-api/internal/app/repositories"
+)
+
+type PromotionServiceImpl struct {
+	Repository *repositories.PromotionRepository
+}
+
+func NewPromotionService(promotionRepo *repositories.PromotionRepository) *PromotionServiceImpl {
+	return &PromotionServiceImpl{
+		Repository: promotionRepo,
+	}
+}
+
+func (s *PromotionServiceImpl) CreatePromotion(promo models.Promotion) (models.Promotion, error) {
+	panic("implement me!")	
+}
+
+func (s *PromotionServiceImpl) GetAllPromotions() ([]models.Promotion, error) {
+	return s.GetAllPromotions()
+}
+
+func (s *PromotionServiceImpl) GetPromotionbyPromotionID(promotionID string) (models.Promotion, error) {
+	panic("implement me!")	
+}
+
+func (s *PromotionServiceImpl) UpdatePromotionbyPromotionID(promo models.Promotion) (models.Promotion, error) {
+	panic("implement me!")	
+}
+
+func (s *PromotionServiceImpl) DeletePromotionbyPromotionID(promotionID string) error {
+	panic("implement me!")	
+}
